1e18cc3312c2c1ba03bd9fb29abf697e485b200d
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-11-05 17:55:41 +0100

    update example

--- example.py
+++ example.py
@@ -20,7 +20,7 @@ import services.service_helper as shelp
 # slog.initialise_log_file_with_headers(slog.create_headers(4), save_path)
 # slog.log_results_to_csv(vals, save_path, prepare=True)
 
-postfix = "_own"
+postfix = ""
 
 print(postfix)
 
@@ -36,6 +36,7 @@ for i in range(10):
                         tmax=1000, 
                         density=0.01, 
                         number_particles=10,
+                        noise_percentage=0,
                         add_own_orientation=True,
                         add_random=False, 
                         num_generations=20, 
