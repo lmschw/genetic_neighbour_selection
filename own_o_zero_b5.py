41d073d5510a84fe77347b7cbeb0038abef10eff
lmschw <lillyschwarzenbach@gmail.com>
2024-11-06 18:59:50 +0100

    update examples

--- own_o_zero_b5.py
+++ own_o_zero_b5.py
@@ -6,21 +6,8 @@ import services.service_preparation as sprep
 import services.service_logging as slog
 import services.service_helper as shelp
 
-
-# c_values = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0]) # farthest
-
-# noise = sprep.get_noise_amplitude_value_for_percentage(1)
-# simulator = RunModel(domain_size=(10, 10), radius=20, noise=noise, speed=1, number_particles=4, c_values=c_values)
-# simulator.simulate(tmax=3000)
-
-# vals = [{'iter': 0, 'ind_idx': 0, 'individual': np.array([0, 0, 0, 0, 0, 1, 0, 0, 0]), 'fitness': 20},
-#         {'iter': 1, 'ind_idx': 2, 'individual': np.array([0.2, 0.2, 1, 0, 0, 0.2, 0, 0, 0.1]), 'fitness': 30}]
-
-# save_path = "test.csv"
-# slog.initialise_log_file_with_headers(slog.create_headers(4), save_path)
-# slog.log_results_to_csv(vals, save_path, prepare=True)
-
-postfix = "_own_disorder"
+n = 10
+postfix = f"_own_order_zero_n={n}_b5"
 
 print(postfix)
 
@@ -29,9 +16,10 @@ save_path_best_normalised = f"best{postfix}_normalised.csv"
 save_path_general = f"all{postfix}"
 save_path_plot = f"plot{postfix}"
 
-n = 10
 add_own = True
 add_random = False
+bounds = [-5, 5]
+zero_bounds = [-0.5, 0.5]
 
 num_c_values = 3 * (n-1)
 if add_own:
@@ -43,7 +31,7 @@ if add_random:
 slog.initialise_log_file_with_headers(slog.create_headers(num_c_values, is_best=True), save_path=save_path_best)
 slog.initialise_log_file_with_headers(slog.create_headers(num_c_values, is_best=True), save_path=save_path_best_normalised)
 
-for i in range(1):
+for i in range(50):
     model = GeneticModel(radius=100, 
                         tmax=1000, 
                         density=0.01, 
@@ -54,8 +42,10 @@ for i in range(1):
                         num_generations=20, 
                         num_iterations_per_individual=10,
                         population_size=20,
+                        bounds=bounds,
+                        update_to_zero_bounds=zero_bounds,
                         early_stopping_after_gens=None,
-                        target_order=0)
+                        target_order=1)
 
     best = model.run(save_path_log=save_path_general, save_path_plots=save_path_plot)
     print(f"BEST overall: {best}")
