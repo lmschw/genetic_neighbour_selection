64b7723e6ec93666b1a0fe216ca99fbceb79db00
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-11-08 12:10:47 +0100

    set normalisation default to L1

--- services/service_helper.py
+++ services/service_helper.py
@@ -4,7 +4,7 @@ from sklearn import preprocessing
 Contains helper methods that do not fit the themes of the other services.
 """
 
-def normalise(values, norm='l2'):
+def normalise(values, norm='l1'):
     """
     Applies the Euclidean norm to the values.
 
