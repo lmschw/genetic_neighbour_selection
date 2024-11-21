f7ca3274ff6d103db12f6e336bc129581c045733
lmschw <lillyschwarzenbach@gmail.com>
2024-11-21 08:03:53 +0100

    specify number of clusters on top

--- eval.py
+++ eval.py
@@ -12,23 +12,26 @@ from sklearn.metrics import davies_bouldin_score, silhouette_score
 
 #location = "results/own_order_basic_n=5/"
 #base_filepath = f"{location}best_own_order_basic_n=5_b5_normalised"
-base_filepath = "best_own_random_order_basic_n=10_b5_normalised"
+base_filepath = f"results/19.11.24/best_test_ga_disorder_n=10_pi=0.9_pm=0.1_pn=0.05_g=20_pop=30_noise=1_speed=0.1_normalised"
 
 df = pd.read_csv(f"{base_filepath}.csv")
+
+n_clusters = 4
 
 print(df.head())
 
 print(f"avg fitness: {np.average(df['fitness'])}, min: {np.min(df['fitness'])}, max: {np.max(df['fitness'])}")
-cols = {f"individual_{i}": f"{i}" for i in range(len(df.columns)-2)}
+cols = {f"individual_{i}": f"{i}" for i in range(len(df.columns)-3)}
 df = df.rename(columns=cols)
-data = df.drop(columns=['iter', 'fitness'])
+data = df.drop(columns=['iter', 'fitness', 'fitness_order'])
 
-plt.plot(data)
+plt.plot(data.T)
 plt.savefig(f"{base_filepath}_full_line.jpeg")
 
 
+"""
 plt.figure(figsize = (10,7))
-idx_list = [2, 4, 3, 0]
+idx_list = [20, 16, 4, 23]
 example_data = data.iloc[idx_list]
 plt.plot(example_data.T)
 plt.legend(idx_list)
@@ -36,13 +39,13 @@ plt.savefig(f"{base_filepath}_clusters.jpeg")
 plt.show()
 
 plt.figure(figsize = (10,7))
-idx_list = [11, 0, 1]
+idx_list = [16, 22]
 example_data = data.iloc[idx_list]
 plt.plot(example_data.T)
 plt.legend(idx_list)
 plt.savefig(f"{base_filepath}_cluster_line_similar.jpeg")
 plt.show()
-
+"""
 
 pca = PCA(n_components=2)
 pca.fit(data)
@@ -63,7 +66,7 @@ dend = sch.dendrogram(sch.linkage(df_pca, method='ward'))
 plt.savefig(f"{base_filepath}_dendrogram.svg")
 plt.savefig(f"{base_filepath}_dendrogram.jpeg")
 
-model = AgglomerativeClustering(n_clusters = 2)
+model = AgglomerativeClustering(n_clusters = n_clusters)
 y_means = model.fit_predict(df_pca)     
 plt.figure(figsize=(10, 8))
 plt.title('Agglomerative Clustering with 2 Clusters')
