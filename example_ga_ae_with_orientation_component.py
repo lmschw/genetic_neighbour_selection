3f3ac7d32cccd76fbeb90f3154335646580687ce
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-12-12 16:30:29 +0100

    add orientation component to active elastic implementation

---
+++ example_ga_ae_with_orientation_component.py
@@ -0,0 +1,129 @@
+import numpy as np
+
+from model.active_elastic.genetic_algorithm_model_active_elastic_with_orientation_component import GeneticAlgorithmActiveElasticWithOrientationComponent
+import services.service_preparation as sprep
+import services.service_logging as slog
+import services.service_helper as shelp
+
+n = 10
+alpha = 0.005
+prob_init = 0.9
+prob_mut = 0.1
+prob_intro = 0.05
+start_order = 0
+target_order = 1
+population_size = 30
+num_gens = 20
+num_iters = 1
+num_iters_per_ind = 1
+early_stopping = None
+
+radius = 10
+noise_percentage = 1
+density = 0.01
+tmax = 1000
+
+
+bounds = [-5, 5]
+
+num_c_values = n
+
+if target_order == 1:
+    postfix = f"_test_ga_ae_with_orient_order_n={n}_pi={prob_init}_pm={prob_mut}_g={num_gens}_pop={population_size}"
+elif target_order == 0:
+    postfix = f"_test_ga_ae_with_orient_disorder_n={n}_pi={prob_init}_pm={prob_mut}_g={num_gens}_pop={population_size}"
+else:
+    postfix = f"_test_ga_ae_with_orient_middle_n={n}_pi={prob_init}_pm={prob_mut}_g={num_gens}_pop={population_size}"
+
+print(postfix)
+
+save_path_best = f"best{postfix}.csv"
+save_path_best_normalised = f"best{postfix}_normalised.csv"
+save_path_general = f"all{postfix}"
+save_path_plot = f"plot{postfix}"
+
+slog.initialise_log_file_with_headers(slog.create_headers(num_c_values, is_best=True, n=n, has_own=False, has_random=False, ranking_by=[False, False, False]), save_path=save_path_best)
+slog.initialise_log_file_with_headers(slog.create_headers(num_c_values, is_best=True, n=n, has_own=False, has_random=False, ranking_by=[False, False, False]), save_path=save_path_best_normalised)
+
+for i in range(num_iters):
+    model = GeneticAlgorithmActiveElasticWithOrientationComponent(radius=radius, 
+                        tmax=tmax, 
+                        density=density, 
+                        number_particles=n,
+                        noise_percentage=noise_percentage,
+                        bounds=bounds,
+                        c_values_norm_factor=alpha,
+                        zero_choice_probability_initial=prob_init,
+                        zero_choice_probability_mutation=prob_mut,
+                        introduce_new_values_probability=prob_intro,
+                        num_generations=num_gens, 
+                        num_iterations_per_individual=num_iters_per_ind,
+                        population_size=population_size,
+                        early_stopping_after_gens=early_stopping,
+                        start_order=start_order,
+                        target_order=target_order)
+
+    best = model.run(save_path_log=save_path_general, save_path_plots=save_path_plot)
+    print(f"BEST overall: {best}")
+
+
+    slog.log_results_to_csv([{'iter': i, 'individual': np.array(best[0]), 'fitness': best[1], 'fitness_order': best[2]}], ranking_by=[False, False, False], prepare=True, save_path=save_path_best, n=n)
+    slog.log_results_to_csv([{'iter': i, 'individual': shelp.normalise(np.array(best[0])), 'fitness': best[1], 'fitness_order': best[2]}], ranking_by=[False, False, False], prepare=True, save_path=save_path_best_normalised, n=n)
+
+
+
+
+
+""" import numpy as np
+
+from model.run_model_active_elastic_basic import ActiveElasticRunModel
+import services.service_preparation as sprep
+import services.service_logging as slog
+import services.service_helper as shelp
+
+from animator.animator_2d import Animator2D
+from animator.animator_matplotlib import MatplotlibAnimator
+from evaluator.evaluator_multi_comp import EvaluatorMultiAvgComp
+
+domain_size = (31, 31)
+n = 100
+radius = 10
+speed = 0.1
+alpha = 0.5
+beta = 0.5
+spring_constant = 0.5
+natural_distance = 10
+sensing_noise = sprep.get_noise_amplitude_value_for_percentage(1)
+actuation_noise = sprep.get_noise_amplitude_value_for_percentage(0.5)
+
+c_values = np.ones(n)
+
+model_params = {'number_particles': n, 'radius': radius, 'domain_size': domain_size}
+tmax = 1000
+save_path = "ae_basic_test"
+
+simulator = ActiveElasticRunModel(domain_size=domain_size, 
+                                  number_particles=n, 
+                                  radius=radius, 
+                                  c_values=c_values,
+                                  speed=speed,
+                                  alpha=alpha,
+                                  beta=beta,
+                                  spring_constant=spring_constant,
+                                  natural_distance=natural_distance,
+                                  sensing_noise=sensing_noise,
+                                  actuation_noise=actuation_noise)
+
+simulation_data = simulator.simulate(tmax=tmax)
+times, positions, orientations = simulation_data
+
+animator = MatplotlibAnimator(simulation_data, (domain_size[0], domain_size[1], 100), red_indices=[0])
+
+# prepare the animator
+#preparedAnimator = animator.prepare(Animator2D(modelParams), frames=modelParams["tmax"])
+preparedAnimator = animator.prepare(Animator2D(model_params), frames=tmax)
+
+preparedAnimator.save_animation(f"{save_path}.mp4")
+
+ """
+
---
+++ model/active_elastic/run_model_active_elastic_with_orientation_component.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+
+import numpy as np
+import os
+import math
+import matplotlib.pyplot as plt
+from scipy.stats import levy_stable, circmean, circvar
+import pickle
+from datetime import datetime
+
+from model.run_model import RunModel
+import services.service_orientations as sorient
+import services.service_helper as shelp
+
+class ActiveElasticRunModel(RunModel):
+    """
+    Performs the actual simulation of the agents interacting in the domain.
+    """
+    def __init__(self, domain_size, number_particles, radius, c_values, speed=1, a=0.5, b=0.5, alpha=0.1, beta=0.1, spring_constant=0.1, natural_distance=1, sensing_noise=0, actuation_noise=0):
+        super().__init__(domain_size=domain_size, radius=radius, noise=sensing_noise, speed=speed, number_particles=number_particles,
+                         c_values=c_values, add_ranking_by=[True, False, False], add_own_orientation=True, add_random=False)
+        self.domain_size = np.array(domain_size)
+        self.number_particles = number_particles
+        self.radius = radius
+        self.c_values = c_values
+        self.speed = speed
+        if a == 0:
+            self.a = a
+            if b == 0:
+                self.b = b
+            else:
+                self.b = 1
+        elif b == 0:
+            self.a = 1
+            self.b = b
+        else:
+            self.a = a / (a + b)
+            self.b = b / (a + b)
+        self.alpha = alpha
+        self.beta = beta
+        self.spring_constant = spring_constant
+        self.natural_distance = natural_distance
+        self.sensing_noise = sensing_noise
+        self.actuation_noise = actuation_noise
+
+    def initialize_state(self):
+        positions = self.domain_size*np.random.rand(self.number_particles,len(self.domain_size))
+        orientations = sorient.normalize_orientations(np.random.rand(self.number_particles, len(self.domain_size))-0.5)
+        return positions, orientations
+    
+    def compute_bearings(self, positions):
+        """
+        Computes the bearings for every particle.
+
+        Params:
+            - positions (np.array): the position of every particle at the current timestep
+
+        Returns:
+            Numpy array containing the bearings of all particles. Values between [-pi, pi]
+        """
+        xDiffs = positions[:,0,np.newaxis]-positions[np.newaxis,:,0]
+        yDiffs = positions[:,1,np.newaxis]-positions[np.newaxis,:,1]
+
+        return np.arctan2(yDiffs, xDiffs)
+    
+    def get_force(self, positions, orientations):
+        neighbours = shelp.get_neighbours(positions=positions, domain_size=self.domain_size, radius=self.radius)
+        neighbours = neighbours[:, :, np.newaxis] + neighbours[:, :, np.newaxis]
+        neighbours = np.repeat(neighbours, 2, axis=2)
+        distances = positions[:, np.newaxis] - positions[np.newaxis, :]
+        distances = np.where((neighbours), distances, 0)
+        spring_constant_div = -self.spring_constant/self.natural_distance
+        distances_norm = np.linalg.norm(distances, axis=2, keepdims=True)
+        distances_norm_inf = np.where(distances_norm == 0, np.inf, distances_norm)
+        delta_distance = distances_norm - self.natural_distance
+        norm_div = distances / distances_norm_inf
+        force_components = spring_constant_div * delta_distance * norm_div
+        c_values = np.repeat(self.c_values[np.newaxis, :, np.newaxis], 2, axis=2)
+        force_components_c_values = force_components * c_values
+        active_elastic_force = np.sum(force_components_c_values, axis=1)
+
+        orientations_force = self.compute_new_orientations(positions, orientations)
+        orientations_force = sorient.normalize_orientations(orientations+self.generate_noise())
+
+        return self.a * active_elastic_force + self.b * orientations_force
+    
+    def update_positions(self, positions, orientations, forces):
+        angles = sorient.compute_angles_for_orientations(orientations=orientations)
+        unit_vector_heading = sorient.get_unit_vectors_for_angles(angles=angles)
+        base = self.speed * unit_vector_heading
+        chis_angles = np.random.random(size=self.number_particles) * 2 * np.pi
+        chis = sorient.get_unit_vectors_for_angles(chis_angles)
+        force_with_noise = forces + (self.sensing_noise * chis)
+        new_pos = base + self.alpha * (force_with_noise * unit_vector_heading) * unit_vector_heading
+        new_pos += positions
+        new_pos += -self.domain_size*np.floor(new_pos/self.domain_size)
+        return new_pos
+
+    def update_orientations(self, positions, orientations, forces):
+        unit_vector_heading_perpendicular = sorient.get_perpendicular_vectors(vectors=orientations)
+        chis = np.random.normal(loc=1, scale=1, size=(self.number_particles, 2))
+        force_with_noise = forces + (self.sensing_noise * chis)
+        new_orients = self.beta * (force_with_noise * unit_vector_heading_perpendicular) + (self.actuation_noise * chis)
+        new_orients = sorient.normalize_orientations(new_orients)
+        return new_orients
+
+    def prepare_simulation(self, initialState, dt, tmax):
+        if any(ele is None for ele in initialState):
+            positions, orientations = self.initialize_state()
+        else:
+            positions, orientations = initialState
+
+        if dt is None and tmax is not None:
+            dt = 1
+        
+        if tmax is None:
+            tmax = (10**3)*dt
+            dt = np.average(10**(-2)*(np.max(self.domain_size)/self.speed))
+
+        self.tmax = tmax
+        self.dt = dt
+
+        # Initialisations for the loop and the return variables
+        self.num_intervals=int(tmax/dt+1)
+
+        self.positions_history = np.zeros((self.num_intervals,self.number_particles,len(self.domain_size)))
+        self.orientations_history = np.zeros((self.num_intervals,self.number_particles,len(self.domain_size)))
+
+        self.positions_history[0,:,:] = positions
+        self.orientations_history[0,:,:] = orientations
+
+        return positions, orientations
+
+    def simulate(self, initialState=(None, None, None), dt=None, tmax=None):
+        positions, orientations = self.prepare_simulation(initialState=initialState, dt=dt, tmax=tmax)
+        for t in range(0, self.tmax, self.dt):
+            self.t = t
+
+            forces = self.get_force(positions=positions, orientations=orientations)
+
+            positions = self.update_positions(positions=positions, orientations=orientations, forces=forces)
+            orientations = self.update_orientations(positions=positions, orientations=orientations, forces=forces)
+
+            self.positions_history[t,:,:]=positions
+            self.orientations_history[t,:]=orientations
+
+        return (self.dt*np.arange(self.num_intervals), self.positions_history, self.orientations_history)
